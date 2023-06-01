b8e97ad1079d04655b8666137d818bd2d6bcac2e
Abinash Kumar Sahoo <ab1nashkumar9815@gmail.com>
2023-06-02 01:51:01 +0530

    props

--- src/Cartitem.js
+++ src/Cartitem.js
@@ -1,17 +1,7 @@
 import React from "react";
 
 class CartItem extends React.Component{
-    constructor (){
-
-        super();
-        this.state ={
-            price: 1099,
-            title : 'Mobile Phone',
-            qty : 1,
-            img : ''
-        }
-        // this.increaseQuantity = this.increaseQuantity.bind(this)
-    }
+    
     increaseQuantity =() =>{
         // this.state.qty += 1;
       
@@ -46,7 +36,8 @@ class CartItem extends React.Component{
 
     
     render() {
-        const { price , title, qty}= this.state;
+        console.log('this.props',this.props);
+        const { price , title, qty}= this.props.product;
 
         return(
          <div className ="cart-item">
--- src/cart.js
+++ src/cart.js
@@ -2,16 +2,53 @@ import React from "react";
 import CartItem from "./Cartitem";
 
 class Cart extends React.Component{
+    constructor (){
+
+        super();
+        this.state ={
+         
+            products :[
+                {   
+                price: 1099,
+                title : 'Mobile Phone',
+                qty : 1,
+                img : '',
+                id: 1
+            },   
+            {
+            price: 10099,
+            title : 'Fax Machine',
+            qty : 41,
+            img : '',
+            id: 2
+        },  
+            { price: 169,
+            title : 'SOAP',
+            qty : 14,
+            img : '',
+            id: 3
+        }
+        ]
+        // this.increaseQuantity = this.increaseQuantity.bind(this)
+    }}
   
     render()
     {
+ const {products} = this.state;
+      
         return (
             <div className="cart">
-            <CartItem />
-            <CartItem />
-            <CartItem />
+            
+            
+            {products.map((product)=> {
+                return (
+                <CartItem 
+                product ={product} 
+                key={product.id}
+                  />)
+            })}
+            
             </div>
-           
             
         );
     }
@@ -19,4 +56,7 @@ class Cart extends React.Component{
 
       
 
-export default Cart;
+
+
+
+export default Cart; 
